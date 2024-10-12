91545c833493467c09bd532a26e939105017e6d8
NeebinNodin <164468785+NeebinNodin@users.noreply.github.com>
2024-10-12 18:40:39 -0500

    Got it working. Before turnings rest of code into functions.

--- NRB.Assignment5/Main.cpp
+++ NRB.Assignment5/Main.cpp
@@ -8,48 +8,72 @@
 #include <vector>
 #include <string>
 
-using namespace std;
+// Function Prototypes
+std::vector<std::string> loadQuestions(const std::string &filepath);
 
 int main()
 {
-	// Math quiz that opens a set of three questions from a Questions.txt
-	vector<string> questions;
-	vector<double> answers;
+	
+	
+	// Math quiz that opens a set of three questions from  Questions.txt
+	// saves the results of three questions to Results.txt
+	// question = answer
 
-	// Open the file
-	string filepath = "Questions.txt";
+	// Variables
+	std::vector<std::string> questions;
+	std::vector<double> answers;
+
+	std::string filepath = "Questions.txt";
+	std::string filepath2 = "Results.txt";
+	
+	const int numQuestions = 3;
+	srand(time(NULL));
+	int randomNumber; 
+	double answer;
+
+	/*std::string line;
+	std::ifstream ifsQuestions(filepath);
+	while (getline(ifsQuestions, line))
+	{
+		questions.push_back(line);
+	}
+	ifsQuestions.close();*/
+	questions = loadQuestions(filepath);
+
+	// Ask 3 questions and answer, save to file Results.txt
+
+	std::cout << "Welcome to the Math Quiz\n\n";
+
+	std::ofstream ofsResults(filepath2);
+	for (int i = 0; i < numQuestions; i++)
+	{
+		randomNumber = rand() % 10 + 1;
+		std::cout << "Question " << (i + 1) << ": " << questions[randomNumber] << "\n";
+		std::cout << "Enter your answer: ";
+		std::cin >> answer;
+		answers.push_back(answer);
+
+		ofsResults << questions[i] << " = " << answer << "\n";
+	}
+	std::cout << "\nResults saved to Results.txt\n";
+	ofsResults.close();
+
 
 	
-	string line;
-	ifstream ifsQuestions(filepath);
+	(void)_getch();
+	return 0;
+}
+
+// Functions
+std::vector<std::string> loadQuestions(const std::string &filepath)
+{
+	std::vector<std::string> questions;
+	std::string line;
+	std::ifstream ifsQuestions(filepath);
 	while (getline(ifsQuestions, line))
 	{
 		questions.push_back(line);
 	}
 	ifsQuestions.close();
-
-	// Ask 3 questions and answer, save to file Results.txt
-
-	string filepath2 = "Results.txt";
-
-	const int numQuestions = 10;
-	srand(time(NULL));
-	int randomNumber; 
-	double answer;
-	ofstream ofsResults(filepath2);
-	for (int i = 0; i < numQuestions; i++)
-	{
-		randomNumber = rand() % 10 + 1;
-		cout << "Question " << (i + 1) << ": " << questions[randomNumber] << "\n";
-		cout << "Enter your answer: ";
-		cin >> answer;
-		answers.push_back(answer);
-
-		ofsResults << questions[randomNumber] << "\n";
-	}
-	cout << "\nResults saved to Results.txt\n";
-	ofsResults.close();
-
-	(void)_getch();
-	return 0;
+	return questions;
 }
